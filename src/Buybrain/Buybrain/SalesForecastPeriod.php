02f54ff71b9e803fd5567f15f6c3c6f7c5f3e97d
Ede Meijer <ede.meijer88@gmail.com>
2017-03-16 12:25:44 +0100

    Add cumulative certainties to sales forecast entity

--- src/Buybrain/Buybrain/SalesForecastPeriod.php
+++ src/Buybrain/Buybrain/SalesForecastPeriod.php
@@ -18,17 +18,25 @@ class SalesForecastPeriod implements JsonSerializable
     private $toDate;
     /** @var SalesForecastQuantityProbability[] */
     private $probabilities;
+    /** @var float[] */
+    private $certainties;
 
     /**
-     * @param DateTimeInterface $fromDate
-     * @param DateTimeInterface $toDate
-     * @param SalesForecastQuantityProbability[] $probabilities
+     * @param DateTimeInterface $fromDate start of the period
+     * @param DateTimeInterface $toDate end of the period, exclusive
+     * @param SalesForecastQuantityProbability[] $probabilities of the sold quantity falling in certain quantity ranges
+     * @param float[] $certainties cumulative certainties that up to a quantity gets sold before the end of this period
      */
-    public function __construct(DateTimeInterface $fromDate, DateTimeInterface $toDate, array $probabilities)
-    {
+    public function __construct(
+        DateTimeInterface $fromDate,
+        DateTimeInterface $toDate,
+        array $probabilities,
+        array $certainties
+    ) {
         $this->fromDate = $fromDate;
         $this->toDate = $toDate;
         $this->probabilities = $probabilities;
+        $this->certainties = $certainties;
     }
 
     /**
@@ -56,6 +64,18 @@ class SalesForecastPeriod implements JsonSerializable
     }
 
     /**
+     * Get the map of the certainties that no more than a specific quantity is sold before the end of the period.
+     * For example, a key of "2" with a value of 0.8125 means that there is a 81.25% chance that no more than 2 items
+     * get sold before the end of the current period, including the amounts sold in previous periods.
+     *
+     * @return float[] certainties between 0.0 and 1.0 indexed by integer quantities
+     */
+    public function getCertainties()
+    {
+        return $this->certainties;
+    }
+
+    /**
      * @return array
      */
     public function jsonSerialize()
@@ -64,9 +84,9 @@ class SalesForecastPeriod implements JsonSerializable
             'from' => DateTimes::format($this->fromDate),
             'to' => DateTimes::format($this->toDate),
             'probabilities' => $this->probabilities,
+            'certainties' => (object)$this->certainties
         ];
     }
-
 
     /**
      * Create a SalesForecastPeriod instance from parsed JSON
@@ -79,7 +99,8 @@ class SalesForecastPeriod implements JsonSerializable
         return new self(
             DateTimes::parse($json['from']),
             DateTimes::parse($json['to']),
-            array_map([SalesForecastQuantityProbability::class, 'fromJson'], $json['probabilities'])
+            array_map([SalesForecastQuantityProbability::class, 'fromJson'], $json['probabilities']),
+            $json['certainties']
         );
     }
 }

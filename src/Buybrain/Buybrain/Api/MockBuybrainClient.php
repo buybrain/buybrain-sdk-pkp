68371783bb23545480f8f49325cb4083848fe71e
Ede Meijer <ede.meijer88@gmail.com>
2017-06-22 14:04:14 +0200

    Support request errors in mock API

--- src/Buybrain/Buybrain/Api/MockBuybrainClient.php
+++ src/Buybrain/Buybrain/Api/MockBuybrainClient.php
@@ -29,6 +29,8 @@ class MockBuybrainClient implements BuybrainClient
     private $defaultCertainty = [0 => 1.0];
     /** @var string|null */
     private $defaultSkuError;
+    /** @var string|null */
+    private $requestError;
     /** @var AdviseResponseSku[] */
     private $skuResponses = [];
 
@@ -149,6 +151,18 @@ class MockBuybrainClient implements BuybrainClient
     }
 
     /**
+     * Configure an error to throw on API requests
+     *
+     * @param string $requestError
+     * @return $this
+     */
+    public function setRequestError($requestError)
+    {
+        $this->requestError = $requestError;
+        return $this;
+    }
+
+    /**
      * Create a new purchase advise report. Depending on how long it takes to generate the full result, the response
      * object may or may not have a 'completed' state. In case it is not completed, use getAdvise() to poll for
      * completion and the final result.
@@ -184,6 +198,9 @@ class MockBuybrainClient implements BuybrainClient
 
     private function respond(MockAdviseState $state)
     {
+        if ($this->requestError !== null) {
+            throw new Exception($this->requestError);
+        }
         $progress = (float)$state->getNumRequests() / $this->requestsBeforeFinish;
         $done = $progress === 1.0;
 
